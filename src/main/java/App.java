88c58e42c531f6870f8bb0d7da59f9e1371c327d
Byron <byronblocker@gmail.com>
2017-05-30 10:54:16 -0700

    add final test and front end logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,15 @@
+import java.io.Console;
+import java.util.List;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("I'm working");
+    Console console = System.console();
+    System.out.println("Enter a number");
+    String userNumber = console.readLine();
+    int intUserNum = Integer.parseInt(userNumber);
+
+    PingJava execute = new PingJava();
+    List<Object> pingJavaResults = execute.runPingJava(intUserNum);
+    System.out.println(pingJavaResults); 
   }
 }
